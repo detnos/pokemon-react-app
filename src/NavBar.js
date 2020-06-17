78f6fb7292e8793a674b9bec436bcae17810d420
Jason Taylor <taylorjason@users.noreply.github.com>
2020-06-17 12:48:20 -0400

    Convert component to functional and add onClick handler to buttons

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <nav className="App-nav">
-        <h3> Pokemon React App</h3>
-        <p>Built with Poke.api</p>
-        <button>View All</button>
-        <button>My Collection</button>
-        <button>Grocery List</button>
-        <button>Search</button>
-      </nav>
-    );
-  }
+function NavBar(props) {
+  return (
+    <nav className="App-nav">
+      <h3> Pokemon React App</h3>
+      <p>Built with Poke.api</p>
+      <button onClick={props.handleClick.bind(this, 'ViewAll')}>
+        View All
+      </button>
+      <button onClick={props.handleClick.bind(this, 'MyCollection')}>
+        My Collection
+      </button>
+      <button onClick={props.handleClick.bind(this, 'GroceryList')}>
+        Grocery List
+      </button>
+      <button onClick={props.handleClick.bind(this, 'Search')}>Search</button>
+    </nav>
+  );
 }
 
 export default NavBar;
