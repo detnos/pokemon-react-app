25c6e846b2383b891ee6ee458120e66f938562b6
Hugh <det.nosnibor00@gmail.com>
2020-06-17 11:44:38 -0500

    Initial changes still not fully functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {PokemonContext} from './PokemonContext';
+import PokemonSearch from './PokemonSearch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Not sure if we need a header or not, or perhaps the nav goes in the header */}
-      </header>
-      <nav className="App-nav">{/* Nav Bar of Features to Click */}</nav>
-      <body className="">{/* Main Body compoenents go here */}</body>
-    </div>
-  );
+//declare variables for fetch process
+let typesObj = {};
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        currentPokemon: {
+          name: ''
+        },
+        groceryList: [
+          { name: '' },
+          { name: '' },
+          { name: '' }
+        ]
+      }
+    }
+
+  static contextType = PokemonContext;
+
+  handleSearch(event) {
+    this.setState({
+      currentPokemon: { name: event.target.value}
+    })
+    event.preventDefault()
+
+    console.log('currentPokemon: ', this.context.currentPokemon.name)
+    // fetch('https://pokeapi.co/api/v2/pokemon/' + event.tagert.value)
+    //   .then(response => response.json()) // turn the response into json
+    //   .then(json => {
+    //     let outputString = json.name + ': '
+    //     let pokeType = json.types;
+    //     pokeType.forEach(typeItem => {
+    //       //push the type to the array where the key is the name of the pokemon
+    //       typesObj[json.name].push(typeItem.type.name)
+    //     });
+    //     //console.log pokemon name : type, type, etc..
+    //     outputString += typesObj[json.name].join(', ');
+    //     console.log(outputString);
+    //   });
+
+
+  }
+
+  handleChange(event) {
+    this.setState({
+      currentPokemon: {
+        name: event.target.value
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <PokemonContext.Provider value={{
+          state: this.state,
+          handleChange: this.handleChange.bind(this),
+          handleSearch: this.handleSearch.bind(this)
+        }}>
+        <header className="App-header">
+          <PokemonSearch />
+          {/* Not sure if we need a header or not, or perhaps the nav goes in the header */}
+        </header>
+        <nav className="App-nav">{/* Nav Bar of Features to Click */}</nav>
+        <body className="">{/* Main Body compoenents go here */}</body>
+        </PokemonContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
