9d56cfa7681f1647bb99ad36f5ed063460cd2086
Hugh <det.nosnibor00@gmail.com>
2020-06-17 16:52:49 -0500

    Functional

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -5,13 +5,41 @@ class Pokemon extends React.Component {
   static contextType = PokemonContext;
 
   render() {
-      
+    let currPokemon = this.context.state.currentPokemon;
+
     return (
-      <div className="Pokemon-display">
-        <p>Pokemon Component</p>
-        <div className="pokemon-image"></div>
-        <p>{this.context.state.currentPokemon.name}</p>
-        {console.log(this.context.state.currentPokemon.sprites.front_default)}
+        
+        <div className="Pokemon-display">
+            {console.log(currPokemon)}
+            <h1>{this.context.state.currentPokemon.name}</h1>
+        <div className="pokemon-image">
+            <img src={currPokemon.sprites.front_default} alt="pokemon-sprite"/>
+        </div>
+        <div className="abilities">
+                <p><strong>Abilities:</strong></p>
+            <ul>
+                {currPokemon.abilities.map(ability => {
+                    return (<li>{ability.ability.name}</li>)
+                })}
+            </ul>
+        </div>
+        <div className="types">
+                <p><strong>Type(s):</strong></p>
+            <ul>
+                {currPokemon.types.map(type => {
+                    return (<li>{type.type.name}</li>)
+                })}
+            </ul>
+        </div>
+        <div className="Stats">
+                <p><strong>Stats:</strong></p>
+            <ul>
+                {currPokemon.stats.map(stat => {
+                    return (<li>{stat.stat.name}: {stat.base_stat}</li>)
+                })}
+            </ul>
+        </div>
+
       </div>
     );
   }
